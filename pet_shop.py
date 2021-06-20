7dd67b69004784ba8ceff19341f5c4aa4f0119e9
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-06-20 19:02:41 +0100

    12th error resolved

--- pet_shop.py
+++ pet_shop.py
@@ -48,3 +48,6 @@ def remove_pet_by_name(pet_shop, Arthur):
         if pet["name"] == Arthur: 
             index_to_remove = pet_shop["pets"].index(pet) 
             pet_shop["pets"].pop(3)
+
+def add_pet_to_stock(pet_shop, new_pet):
+     pet_shop["pets"].append(new_pet)           
