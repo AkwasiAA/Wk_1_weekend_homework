4112238d8f2721c02289f5bc4a7af6f8ab14b984
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-06-20 10:08:39 +0100

    4th error resolved

--- pet_shop.py
+++ pet_shop.py
@@ -5,5 +5,6 @@ def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
 def add_or_remove_cash(pet_shop, cash_figure):
-    pet_shop["admin"]["total_cash"]=(pet_shop["admin"]["total_cash"]+cash_figure)
+    pet_shop["admin"]["total_cash"]=(pet_shop["admin"]["total_cash"]+ cash_figure)
+
 
